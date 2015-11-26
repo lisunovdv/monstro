5601b5b3079292e79a563b4947b6905003ce4e76
faust <lisunovdv@gmail.com>
2015-11-27 00:40:40 +0200

    project common structure created

---
+++ src/main/java/view/fx/MonstroFX.java
@@ -0,0 +1,18 @@
+package view.fx;/**
+ * Created by faust on 27.11.15.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MonstroFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
